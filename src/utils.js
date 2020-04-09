964af6df11457fd4b22d76cdd321ef7420789ccc
wale-bant <wale.ashiru04@gmail.com>
2020-04-09 11:07:49 +0100

    fix - estimate projecton in days

--- src/utils.js
+++ src/utils.js
@@ -1,18 +1,3 @@
-//   get estimated number of infected based on period type
-export const getEstimatedNumberOfInfected = (data) => {
-  if (data.periodType === 'days') {
-    if (data.timeToElapse > 3) return 2 ** Math.trunc(data.timeToElapse / 3);
-  }
-  if (data.periodType === 'weeks') {
-    return 2 ** Math.trunc((data.timeToElapse * 7) / 3);
-  }
-  if (data.periodType === 'months') {
-    return 2 ** Math.trunc((data.timeToElapse * 30) / 3);
-  }
-  return data.timeToElapse;
-};
-
-
 // normalise time to elapse in days based on period type
 export const getTimeToElapse = (data) => {
   if (data.periodType === 'weeks') {
@@ -20,6 +5,16 @@ export const getTimeToElapse = (data) => {
   }
   if (data.periodType === 'months') {
     return data.timeToElapse * 30;
+  }
+  return data.timeToElapse;
+};
+
+//   get estimated number of infected based on period type
+export const getEstimatedNumberOfInfected = (data) => {
+  if (data.periodType === 'days') {
+    if (data.timeToElapse >= 3) return 2 ** Math.trunc(data.timeToElapse / 3);
+  } else {
+    return 2 ** Math.trunc(getTimeToElapse(data) / 3);
   }
   return data.timeToElapse;
 };
